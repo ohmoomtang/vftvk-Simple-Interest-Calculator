f3ebb8acb6ab64e7db55eb32b31852b0352ff84c
Napawan Srisuksawad <ohmoomtang@swanepoel.local>
2021-11-08 00:33:04 +1100

    Done, adjusting style reamining

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var amount = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var displayedYear = new Date().getFullYear()+parseInt(years);
+    var interestObtained = (amount*rate*years)/100;
     
+    document.getElementById("result").innerHTML = 
+    "if you deposit <mark>" + amount + "</mark>,<br />at an interest rate of <mark>" + rate + "%</mark>.<br />You will receive an amount of <mark>" + interestObtained + "</mark>,<br />in the year <mark>" + displayedYear + "</mark>";
+}
+
+function updateInterestRateValue(){
+    var rate = document.getElementById("rate").value;
+    document.getElementById("interestRateValue").innerHTML = parseFloat(rate).toFixed(2) + "%";
 }
         
